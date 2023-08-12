485cbd16d7681b2d4ed814b4e93a720b0c596145
Sidhant Roymoulik <sidhant.roymoulik@gmail.com>
2023-08-12 02:07:36 -0400

    Add increment to TC to stop timeouts

--- ChessEngineTuner/Program.cs
+++ ChessEngineTuner/Program.cs
@@ -181,7 +181,7 @@ namespace ChessEngineTuner
                         // Put your command to CuteChess here
                         "-engine name=\"BotA\" cmd=\"./Chess-Challenge.exe\" arg=\"cutechess uci TunedBotA\" " +
                         "-engine name=\"BotB\" cmd=\"./Chess-Challenge.exe\" arg=\"cutechess uci TunedBotB\" " +
-                        "-each proto=uci tc=3 bookdepth=6 book=./resources/book.bin -concurrency 12 -maxmoves 80 -games 2 -rounds 6 " +
+                        "-each proto=uci tc=3+0.01 bookdepth=6 book=./resources/book.bin -concurrency 12 -maxmoves 80 -games 2 -rounds 6 " +
                         "-ratinginterval 10 -pgnout games.pgn -sprt elo0=0 elo1=20 alpha=0.05 beta=0.05"
                 }
             };
